4eab56241851a66028d8d2e4917d8a8908280f2c
ZeinebTouatiKarmani <zeinebtouati26@gmail.com>
2023-12-14 11:03:13 +0100

    rien

---
+++ index.js
@@ -0,0 +1,20 @@
+let programmingLanguage = document.getElementById("language")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function html() {
+    
+    programmingLanguage.textContent = "With HTML you can create your own Website."
+}
+function css() {
+    
+    programmingLanguage.textContent = "CSS describes how HTML elements should be displayed."
+}
+function javascript() {
+    
+    programmingLanguage.textContent = "JAVASCRIPT makes HTML pages more dynamic and interactive."
+}
+
+
+
+
